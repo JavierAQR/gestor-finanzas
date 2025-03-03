58e515c98a77fa65f5b5c17b07c4ca83a7ef63c4
JavierAQR <nonamesteam123@gmail.com>
2025-03-02 21:24:18 -0500

    agregado echarts para los graficos estadisticos

--- src/Components/Dashboard/StatsDashboard.tsx
+++ src/Components/Dashboard/StatsDashboard.tsx
@@ -1,5 +1,23 @@
+import ReactECharts from "echarts-for-react";
 const StatsDashboard = () => {
-  return <div>StatsDashboard</div>;
+  const options = {
+    title: {
+      text: "Ejemplo de Gráfico con ECharts",
+    },
+    tooltip: {},
+    xAxis: {
+      data: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: "Ventas",
+        type: "bar",
+        data: [5, 20, 36, 10, 10, 20],
+      },
+    ],
+  };
+  return <ReactECharts option={options} />;
 };
 
 export default StatsDashboard;
