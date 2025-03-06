3041b8848dcca92751bbc6e4b2704adc3983a6c8
JavierAQR <nonamesteam123@gmail.com>
2025-03-06 17:20:47 -0500

    styles: Tablas con tamano maximo y scroll vertical

--- src/Components/ReusableTable/ReusableTable.tsx
+++ src/Components/ReusableTable/ReusableTable.tsx
@@ -27,51 +27,53 @@ const ReusableTable = <T,>({
   handleUpdate,
 }: Props<T>) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((column, index) => (
-            <th
-              key={index}
-              className={column.hide === true ? "hide-on-mobile" : undefined}
-            >
-              {column.header}
-            </th>
-          ))}
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {columns.map((column, colIndex) =>
-              column.function ? (
-                column.function(row, colIndex)
-              ) : (
-                <td
-                  key={colIndex}
-                  className={column.hide ? "hide-on-mobile" : ""}
-                >
-                  {String(row[column.key])}
-                </td>
-              )
-            )}
-            <td className="acciones">
-              {handleUpdate && (
-                <button onClick={() => handleUpdate(row)}>
-                  <i className="fa-solid fa-pen"></i>
-                </button>
-              )}
-              <button
-                onClick={() => handleDelete(String(row["id" as keyof T]))}
+    <div className="reusable-table">
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th
+                key={index}
+                className={column.hide === true ? "hide-on-mobile" : undefined}
               >
-                <i className="fa-solid fa-trash"></i>
-              </button>
-            </td>
+                {column.header}
+              </th>
+            ))}
+            <th>Acciones</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map((column, colIndex) =>
+                column.function ? (
+                  column.function(row, colIndex)
+                ) : (
+                  <td
+                    key={colIndex}
+                    className={column.hide ? "hide-on-mobile" : ""}
+                  >
+                    {String(row[column.key])}
+                  </td>
+                )
+              )}
+              <td className="acciones">
+                {handleUpdate && (
+                  <button onClick={() => handleUpdate(row)}>
+                    <i className="fa-solid fa-pen"></i>
+                  </button>
+                )}
+                <button
+                  onClick={() => handleDelete(String(row["id" as keyof T]))}
+                >
+                  <i className="fa-solid fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
